83dcd5f8c75c4f8fcf1783e6a84abbb0643a6851
ali alaa <86577146+alialaabos@users.noreply.github.com>
2022-12-22 08:39:28 +0200

    Update settings.py

--- new/settings.py
+++ new/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'road_map',
         'USER':'postgres',
-        "PASSWORD":'alialaa456',
+        "PASSWORD":'$$$$$$$',
         'HOST':'localhost',
         'PORT':'5432'
 
